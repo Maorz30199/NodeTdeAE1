fccd2edce344bc06e40074998d42226e4e215002
Mauricio Ruiz <maorz@micredi.com.co>
2019-05-19 14:07:58 -0500

    Historia de usuario 2, generar un archivo a partir de la inscripción a un curso

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-const cursos = require('./listaCursos')
+const cursos = require('./listaCursos');
+const fs = require('fs');
+
+const opciones = {
+    id : {
+        demand: true,
+        alias: 'i'
+    },
+    nombre :{
+        demand: true,
+        alias: 'n'
+    },
+    cedula : {
+        demand: true,
+        alias: 'c'
+    }
+}
+
+const argv = require('yargs')
+    .command('inscribir', 'datos: ', opciones)
+    .argv
 
 for (let i = 0; i < cursos.listaCursos.length; i++) {
     setTimeout(() => {
+        // mostrar información del curso cada 2 segundos
        console.log("El Id del Curso es " + cursos.listaCursos[i].id +
        " nombre del Curso es " + cursos.listaCursos[i].nombre +
        " el valor del curso es $" + cursos.listaCursos[i].valor +
        " y la duración es " + cursos.listaCursos[i].duracion + " horas")
     }, i * 2000);
-  }
+}
+
+
+let buscarCurso = cursos.listaCursos.find(function(listaCursos) {
+        return listaCursos.id == argv.i
+    });
+
+let generarArchivo = (listaCursos) =>{
+    texto = 'Hola, ' + argv.n  + '\n' +
+    'Cedula ' + argv.c + '\n' +
+    'Bienvenido al curso ' + listaCursos.nombre + ' Id: '+  listaCursos.id + '\n' +
+    'Duración ' + listaCursos.duracion + ' Valor: '+ listaCursos.valor;
+    fs.writeFile('Incripcion_'+argv.n+'.txt', texto, (err) => {
+        if(err) throw(err);
+    });
+    return 'Generando archivo... \n';
+}  
+
+
+
+console.log(buscarCurso===undefined?"Lo sentimos, no encontramos en curso que buscas, a continuación te mostramos un listado de cursos disponibles: ":generarArchivo(buscarCurso) +
+"Felicitades por tu inscripción al curso: " + buscarCurso.nombre + '\n' +
+"Se ha generado un archivo con la infomación que registraste" + '\n' +
+"A continuación, te mostramos un listado otros cursos disponibles: ")
--- listaCursos.js
+++ listaCursos.js
@@ -2,27 +2,23 @@ listaCursos = [{
     id: 1,
     nombre: 'JavaScript Essential',
     duracion: 20,
-    valor: 300000
+    valor: 300000,
+    alias: 'c1'
 },
 {
     id: 2,
     nombre: 'Scrum',
     duracion: 40,
-    valor: 350000
+    valor: 350000,
+    alias: 'c2'
 },
 {
     id: 3,
     nombre: 'Nodejs Avanzado',
     duracion: 35,
-    valor: 400000
+    valor: 400000,
+    alias: 'c3'
 }]
-
-
-    setTimeout(function(){
-        listaCursos
-    }, 2000)
-
-console.log(listaCursos, listaCursos.length)
 
 module.exports = {
     listaCursos
